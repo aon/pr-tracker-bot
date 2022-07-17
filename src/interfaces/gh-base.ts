0c4b6956e2eb3952a0db52452035384613c1b5af
aon <21188659+aon@users.noreply.github.com>
2022-07-17 13:44:44 -0300

    Split gh interfaces

---
+++ src/interfaces/gh-base.ts
@@ -0,0 +1,39 @@
+export interface User {
+  login: string;
+  id: bigint;
+  avatar_url: string;
+  html_url: string;
+}
+
+export interface PullRequest {
+  id: bigint;
+  html_url: string;
+  diff_url: string;
+  state: "open" | "closed";
+  title: string;
+  body: string;
+  number: number;
+  created_at: string;
+  updated_at: string;
+  closed_at: string;
+  merged_at: string;
+  assignees: User[];
+  requested_reviewers: User[];
+  merged: boolean;
+  merged_by: User;
+}
+
+export interface Repository {
+  id: bigint;
+  name: string;
+  full_name: string;
+  html_url: string;
+}
+
+export interface Review {
+  id: bigint;
+  user: User;
+  state: "APPROVED" | "CHANGES_REQUESTED" | "COMMENTED" | "DISMISSED" | "PENDING";
+}
+
+export interface Organization {}
--- src/interfaces/gh-webhooks.ts
+++ src/interfaces/gh-webhooks.ts
@@ -1,32 +1,19 @@
-export interface User {
-  login: string;
-  id: bigint;
-}
+import { PullRequest, Repository, Organization, Review, User } from "./gh-base";
 
 export interface PullRequestWebhook {
   action: string;
   number: number;
-  pull_request: {
-    id: bigint;
-    html_url: string;
-    diff_url: string;
-    state: "open" | "closed";
-    title: string;
-    body: string;
-    number: number;
-    created_at: string;
-    updated_at: string;
-    closed_at: string;
-    merged_at: string;
-    assignees: User[];
-    requested_reviewers: User[];
-  };
-  repository: {
-    id: bigint;
-    name: string;
-    full_name: string;
-    html_url: string;
-  };
-  organization?: object;
-  sender: object;
+  pull_request: PullRequest;
+  repository: Repository;
+  organization?: Organization;
+  sender: User;
+}
+
+export interface PullRequestReviewWebhook {
+  action: "submitted" | "edited" | "dismissed";
+  pull_request: PullRequest;
+  review: Review;
+  repository: Repository;
+  organization?: Organization;
+  sender: User;
 }
